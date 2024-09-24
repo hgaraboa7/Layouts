d1ff4bd8c3709c6dab2cb5988d780d3adb292b25
hgaraboa7 <hectorgaraboa777@gmail.com>
2024-09-24 17:52:43 +0200

    probando

---
+++ app/src/main/java/com/hectorgc/ejemploslayout/Ejercicio6.java
@@ -0,0 +1,33 @@
+package com.hectorgc.ejemploslayout;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.CheckBox;
+import android.widget.RadioButton;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class Ejercicio6 extends AppCompatActivity implements View.OnClickListener{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_ejercicio6);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+    }
+
+    @Override
+    public void onClick(View view) {
+
+    }
+}
