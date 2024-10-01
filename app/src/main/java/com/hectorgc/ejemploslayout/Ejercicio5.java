e149cc308fe926be6155c0a1b961bd7d2ffb9091
hgaraboa7 <hectorgaraboa777@gmail.com>
2024-10-01 17:04:57 +0200

    probando

--- app/src/main/java/com/hectorgc/ejemploslayout/Ejercicio5.java
+++ app/src/main/java/com/hectorgc/ejemploslayout/Ejercicio5.java
@@ -1,11 +1,6 @@
 package com.hectorgc.ejemploslayout;
 
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
-import android.widget.CheckBox;
-import android.widget.RadioButton;
-import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -13,35 +8,17 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class Ejercicio6 extends AppCompatActivity implements View.OnClickListener{
+public class Ejercicio5 extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_ejercicio6);
+        setContentView(R.layout.activity_ejercicio5);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
-
-
-        Button btn=findViewById(R.id.button3);
-
-        btn.setOnClickListener(this);
-
-
     }
-
-
-
-
-
-    @Override
-    public void onClick(View view) {
-        Toast.makeText(view.getContext(), "boton pulsado", Toast.LENGTH_SHORT).show();
-
-    }
-
 }
